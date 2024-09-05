7dfb4d0a4b9c51beab0b29f6dc89df83e6d8c4ec
Carmelo Maita <carmeloaugustomaitaorlando@gmail.com>
2024-09-05 14:08:27 -0400

    tried to fix another error

--- flake.nix
+++ flake.nix
@@ -52,9 +52,9 @@
         modules = [
           ./hosts/capybara/configuration.nix
           ./hm-modules/user-chiguire.nix
-	  ./nixos-modules/essentials.nix
-	  ./nixos-modules/desktop-essentials.nix
-	  ./nixos-modules/programs-bundle.nix
+	  #./nixos-modules/essentials.nix
+	  #./nixos-modules/desktop-essentials.nix
+	  #./nixos-modules/programs-bundle.nix
           /home/chiguire/.wireguard/kerodon/wireguard-kerodon-0.nix
 	  inputs.stylix.nixosModules.stylix
         ];
