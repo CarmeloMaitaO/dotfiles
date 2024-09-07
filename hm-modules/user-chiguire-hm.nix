533eeff052a21a82881cffdc4d4744952ed68945
Carmelo Maita <carmeloaugustomaitaorlando@gmail.com>
2024-09-07 00:12:19 -0400

    Fix

--- hm-modules/user-chiguire-hm.nix
+++ hm-modules/user-chiguire-hm.nix
@@ -84,5 +84,31 @@
       yt-dlp
       inputs.zen-browser.packages."${system}".generic
     ];
+    stylix = {
+      enable = true;
+      image = inputs.bg-chiguire;
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+      fonts = {
+        serif = {
+          package = pkgs.dejavu_fonts;
+          name = "DejaVu Serif";
+        };
+
+        sansSerif = {
+          package = pkgs.dejavu_fonts;
+          name = "DejaVu Sans";
+        };
+
+        monospace = {
+          package = pkgs.dejavu_fonts;
+          name = "DejaVu Sans Mono";
+        };
+
+        emoji = {
+          package = pkgs.noto-fonts-emoji;
+          name = "Noto Color Emoji";
+        };
+      };
+    };
   };
 }
