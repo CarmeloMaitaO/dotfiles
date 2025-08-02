22799e0957775b61d4a7d3e34bb9d1275d716177
Carmelo Maita <carmeloaugustomaitaorlando@gmail.com>
2025-08-02 14:16:24 -0400

    fix

--- hosts/nixos-pi/configuration.nix
+++ hosts/nixos-pi/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, inputs, modulesPath, ... }:
+{ config, pkgs, inputs, modulesPath, fjordlauncher, ... }:
 {
   imports =
     [ 
