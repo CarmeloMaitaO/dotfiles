260e873904bd1d36796a1a940c058207b70e19ad
Carmelo Maita <carmeloaugustomaitaorlando@gmail.com>
2025-08-02 18:18:23 -0400

    updated printing.nix

--- hosts/nix-modules/printing.nix
+++ hosts/nix-modules/printing.nix
@@ -8,23 +8,25 @@
     # Printing 
     services.printing = {
       enable = true;
-      drivers = [
-        pkgs.gutenprint
-        pkgs.gutenprintBin
-        pkgs.hplip
-        pkgs.hplipWithPlugin
-        pkgs.postscript-lexmark
-        pkgs.samsung-unified-linux-driver
-        pkgs.splix
-        pkgs.foomatic-db
-        pkgs.foomatic-db-engine
-        pkgs.foomatic-db-ppds
-        pkgs.foomatic-db-nonfree
-        pkgs.foomatic-db-ppds-withNonfreeDb
-        pkgs.brlaser
-        pkgs.brgenml1lpr
-        pkgs.brgenml1cupswrapper
-        pkgs.cnijfilter2
+      drivers = with pkgs; [
+        cups-filters
+        cups-browsed
+        gutenprint
+        gutenprintBin
+        hplip
+        hplipWithPlugin
+        postscript-lexmark
+        samsung-unified-linux-driver
+        splix
+        foomatic-db
+        foomatic-db-engine
+        foomatic-db-ppds
+        foomatic-db-nonfree
+        foomatic-db-ppds-withNonfreeDb
+        brlaser
+        brgenml1lpr
+        brgenml1cupswrapper
+        cnijfilter2
       ];
     };
     environment = {
@@ -52,6 +54,7 @@
     services.avahi = {
       enable = true;
       nssmdns4 = true;
+      openFirewall = true;
     };
   };
 }
